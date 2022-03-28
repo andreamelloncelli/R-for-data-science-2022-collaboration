b00020f6bd094bd29b42537a1184c2e89efd59a9
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2022-03-28 10:00:22 +0200

    package setup

---
+++ README.md
@@ -0,0 +1,26 @@
+
+# myPackage
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of myPackage is to ...
+
+## Installation
+
+You can install the development version of myPackage from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools")
+devtools::install_github("andreamelloncelli/R-for-data-science-2022-collaboration")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(myPackage)
+## basic example code
+```
+
---
+++ dev/08_package_setup.R
@@ -0,0 +1,98 @@
+## Create a new package with RStudio
+
+# Package setup -----------------------------------------------------------
+
+## Use version control
+install.packages("usethis")
+usethis::use_git_config(
+  scope = "project",
+  user.name = "John Doe",
+  user.email = "john@example.org"
+)
+usethis::use_git()
+
+
+# * create development folder -----------------------------------------------
+
+# avoid problem with the dev scripts: dev/package-utility.R (this file)
+dir.create("dev")
+# save this file in `dev` as `setup.R`
+usethis::use_build_ignore("dev")
+# now you can save or move this file in "dev"
+
+
+# * Fill in the DESCRIPTION file ---------------------------------------------
+
+# rstudioapi::navigateToFile( "DESCRIPTION" )
+usethis::use_description(
+  list(
+    Title = "My beautiful app",
+    `Authors@R` = "person('Andrea', 'Melloncelli', email = 'andrea@vanlog.it', role = c('cre', 'aut'))",
+    Description = "A sentence describing the package.",
+    URL = "https://github.com/andreamelloncelli/R-for-data-science-2022-collaboration"
+  )
+)
+usethis::use_lgpl_license()       # You can set another license here
+usethis::use_tidy_description()   # sort fields and packages
+
+## * Common tasks -----------------------------------------------------------
+usethis::use_readme_md( open = FALSE )
+# usethis::use_code_of_conduct()
+# usethis::use_lifecycle_badge( "Experimental" )
+# usethis::use_news_md( open = FALSE )
+
+
+## Use tests: if you want to use tests
+# usethis::use_testthat()
+# installed.packages("devtools")
+
+
+# Develop -----------------------------------------------------------------
+
+
+# * Add a dependency ------------------------------------------------------
+
+usethis::use_package( "dplyr" )
+# remeber to add it to ROXYGEN or NAMESPACE:
+#' @import dplyr  # ROXYGEN
+#' import(dplyr)  # NAMESPACE
+
+
+# * Enable Roxygen --------------------------------------------------------
+
+## If you want to use roxygen, enable ROXYGEN in the project.
+# Menu: tools > Project options > build tools > generate the documentation with roxygen
+usethis::use_namespace(roxygen = TRUE)
+devtools::document() # to fill NAMESPACE and documentation with ROXYGEN comments
+# or roxygen2::roxygenise() # converts roxygen comments to .Rd files.
+# or [Ctrl + Shift + D] in RStudio
+
+# * Build or load ---------------------------------------------------------
+# Load the package [CTRL + SHIFT + L] or install-and-reload [CTRL + SHIFT + B]
+
+## Check the package for Cran or [CTRL + SHIFT + E]
+devtools::check(document = FALSE) # check the package
+
+# * Add internal datasets -------------------------------------------------
+## If you want to provide data along with your package
+usethis::use_data_raw( name = "my_dataset", open = FALSE )
+
+# * Add Tests -------------------------------------------------------------
+## Add one line by test you want to create
+usethis::use_test( "hello" )
+
+# * Add Vignettes --------------------------------------------------------
+usethis::use_vignette("ThisTidyPackage")
+devtools::build_vignettes()
+# Install the package and see it with `vignette("ThisTidyPackage")`
+# List your vignettes: vignette(package = 'cancRFDS')
+# Install your package manually: devtools::install(build_vignettes = TRUE)
+
+
+# Deploy ------------------------------------------------------------------
+
+# devtools::missing_s3()
+#
+# devtools::check()
+# rhub::check_for_cran()
+
